4863fe434309d7c89ffbdca42663ebe81e2933a1
Goodusers <53936128+Goodusers@users.noreply.github.com>
2023-04-25 08:14:13 +0500

    Add files via upload

---
+++ New_about.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>О нас</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Montserrat:wght@100;500&display=swap" rel="stylesheet">
+
+</head>
+<body>
+	<header>
+		<div class="header">
+			<div class="header__logo">
+				<div class="img_logo"><a href="#"><img src="img/logo.png"></div></a>
+				<div class="text-logo"><a href="#"> EVERYDAY`S <span> BEAUTY</span></div></a>
+			</div>
+			<div class="profile">
+				<img src="img/profile.png">
+			</div>
+			<div class="application">
+				<a href="#">
+				<h1>ОСТАВИТЬ ЗАЯВКУ</h1></a>
+			</div>
+		</div>
+		<div class="header_second">
+			<div class="header_button"><a href="">ПОРТФОЛИО</a></div>
+			<div class="header_button"><a href="">КАТАЛОГ</a></div>
+			<div class="header_button"><a href="">О НАС</a></div>
+		</div>
+	</header>
+
+		<div class="text_find">ГДЕ НАС НАЙТИ?</div>
+
+		<div class="map">
+			<img src="img/map.png" height="500px" width="1000px">
+		</div>
+
+		<div class="under_map">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+			Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+			 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		</div>
+
+		<div class="text_brand">ТОЛЬКО КАЧЕСТВЕННЫЕ БРЕНДЫ</div>
+
+		<div class="brand"></div>
+
+		<!-- <div class="gallery">
+			<div class="img-wrapper">
+				<img src="img/bandi.jpg" height="200px" width="200px">
+				<img src="img/briggite.jpg" height="200px" width="200px">
+				<img src="img/essie.jpg" height="200px" width="200px">
+				<img src="img/limoni.jpg" height="200px" width="200px">
+				<img src="img/masura.jpg" height="200px" width="200px">
+				<img src="img/opi.jpg" height="200px" width="200px">
+				<img src="img/solargel.jpg" height="200px" width="200px">
+				<img src="img/tint.jpg" height="200px" width="200px">
+			</div>
+		</div> -->
+		
+		<div class="text_master">ТОЛЬКО ЛУЧШИЕ МАСТЕРА</div>
+
+		<div class="wrapper">
+			<div class="info_block">
+				<div class="img_block">
+					<div class="img_master"><img src="img/master.png" alt="maste"></div>
+					<span class="name">Артурия Уретровна</span>
+				</div>
+				<div class="text_block">
+					<span class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex autem nesciunt corrupti labore numquam eius earum ea blanditiis mollitia incidunt necessitat "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</span>
+				</div>
+			</div>
+			<div class="info_block">
+				<div class="text_block">
+					<span class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex autem nesciunt corrupti labore numquam eius earum ea blanditiis mollitia incidunt necessitat "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</span>
+				</div>
+				<div class="img_block">
+					<div class="img_master"><img src="img/master.png" alt="maste"></div>
+					<span class="name">Артурия Уретровна</span>
+				</div>
+			</div>
+			<div class="info_block">
+				<div class="img_block">
+					<div class="img_master"><img src="img/master.png" alt="maste"></div>
+					<span class="name">Артурия Уретровна</span>
+				</div>
+				<div class="text_block">
+					<span class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex autem nesciunt corrupti labore numquam eius earum ea blanditiis mollitia incidunt necessitat "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</span>
+				</div>
+			</div>
+			<div class="info_block">
+				<div class="text_block">
+					<span class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex autem nesciunt corrupti labore numquam eius earum ea blanditiis mollitia incidunt necessitat "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</span>
+				</div>
+				<div class="img_block">
+					<div class="img_master"><img src="img/master.png" alt="maste"></div>
+					<span class="name">Артурия Уретровна</span>
+				</div>
+			</div>
+		</div>
+	
+
+
+
+		
+		
+			
+		
+
+
+
+
+
+</body>
+</html>
